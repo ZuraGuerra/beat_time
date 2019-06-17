baf359eedcf8ec29ed10647351de412cb6700975
Zura Guerra <guerrazuramx@gmail.com>
2019-06-17 13:07:17 -0500

    Refactor main module, add tests

--- lib/beat_time.ex
+++ lib/beat_time.ex
@@ -13,41 +13,36 @@ defmodule BeatTime do
 
   @doc """
   Returns the current .beat time.
-
-  ## Examples
-
-      iex> BeatTime.now()
-      %BeatTime{300}
   """
   @spec now() :: BeatTime.t(integer)
   def now() do
-    current =
-      DateTime.utc_now()
-      |> Dumballah.Convert.to_unix()
-      |> Dumballah.Calculate.add_time(:hours, 1) # UTC+01
+    DateTime.utc_now()
+    |> Dumballah.Convert.to_unix()
+    |> to_beats()
+  end
 
+  @doc """
+  Returns the .beat time representation of a given UNIX time.
+  """
+  @spec to_beats(integer) :: BeatTime.t(integer)
+  def to_beats(current) do
     beats =
       current
+      |> Dumballah.Calculate.add_time(:hours, 1) # UTC+01
       |> calculate_seconds_since_midnight()
-      |> seconds_to_beats()
+      |> Kernel.*(1000)
+      |> div(@beat)
 
     %BeatTime{value: beats}
   end
 
   @doc """
   Returns the text representation of a .beat time.
-
-  ## Examples
-
-      iex> BeatTime.now() |> BeatTime.format()
-      "@300"
   """
   @spec format(BeatTime.t(integer)) :: String.t()
   def format(beat_time), do: "@#{beat_time.value}"
 
   defp to_date_tuple(datetime), do: {datetime.year, datetime.month, datetime.day}
-
-  defp seconds_to_beats(seconds), do: (seconds * 1000) |> div(@beat)
 
   defp calculate_seconds_since_midnight(unix_time) do
     day_start =
--- test/beat_time_test.exs
+++ test/beat_time_test.exs
@@ -2,7 +2,17 @@ defmodule BeatTimeTest do
   use ExUnit.Case
   doctest BeatTime
 
-  test "greets the world" do
-    assert BeatTime.hello() == :world
+  test "returns time in beats" do
+    unix_time = 1560794022
+    expected_beats = 787
+
+    assert BeatTime.to_beats(unix_time) == %BeatTime{value: expected_beats}
+  end
+
+  test "formats beat time" do
+    beats = 787
+    beat_time = %BeatTime{value: beats}
+
+    assert BeatTime.format(beat_time) == "@#{beats}"
   end
 end
